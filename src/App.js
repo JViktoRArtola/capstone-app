4301562fd44fa4cbc32c106f601baa0b2738fddf
tesla <jenviktor13@gmail.com>
2023-12-07 15:32:45 -0500

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from "./Main";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layouts/Layout/Layout";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Menu from "./pages/Menu/Menu";
+import Orders from "./pages/Orders/Orders";
+import Login from "./pages/Login/Login";
+import Reservations from "./pages/Reservations/Reservations";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route index element={<Home/>}/>
+                <Route path="about" element={<About/>}/>
+                <Route path="menu" element={<Menu/>}/>
+                <Route path="reservations" element={<Reservations/>}/>
+                <Route path="orders" element={<Orders/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path="*" element={<Home/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
