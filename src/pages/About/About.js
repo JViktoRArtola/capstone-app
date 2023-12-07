1b0014edf1b23a8d895a1a416243837be323f0d6
tesla <jenviktor13@gmail.com>
2023-12-07 15:59:02 -0500

    Add About page

---
+++ src/pages/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css'; // Assuming you have a CSS file for styling
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <h1>About Little Lemon</h1>
+            <p>Welcome to Little Lemon, a unique place where flavor and freshness come together. Our mission is to provide exceptional food experiences with a special focus on fresh, locally sourced ingredients.</p>
+            <p>Founded in 2021, Little Lemon has quickly become a favorite spot for food lovers seeking something truly special. Our menu is a vibrant mix of classic dishes and innovative culinary experiments, all designed to delight the senses.</p>
+            <p>At Little Lemon, we believe in the power of great food to bring people together. Whether you're joining us for a casual meal or a special celebration, we're committed to making your experience memorable.</p>
+            <p>Thank you for choosing Little Lemon. We look forward to serving you!</p>
+        </div>
+    );
+}
+
+export default About;
